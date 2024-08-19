baf5592e517aad8aa254eb54cd962184747404e6
Manoram <manoramsubedi7@gmail.com>
2024-08-19 19:38:36 +0545

    db image update

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,12 +8,12 @@ from app import calc
 
 class CalcTest(SimpleTestCase):
     def test_add_num(self):
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
 
     def test_sub_num(self):
-        res = calc.sub(10,15)
+        res = calc.sub(10, 15)
 
         self.assertEqual(res, 5)
