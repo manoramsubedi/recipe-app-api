3eb40a1aa9a6509736063908bbc28ab14d584fd7
Manoram <manoramsubedi7@gmail.com>
2024-08-19 19:34:16 +0545

    db image

---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+
+"""
+Sample Test"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    def test_add_num(self):
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+
+
+    def test_sub_num(self):
+        res = calc.sub(10,15)
+
+        self.assertEqual(res, 5)
